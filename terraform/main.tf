3de72c01c2c34f4892e3eec973d0039fa6dcb3f1
Abdu-khaled <abdelrahmankhalid27@gmail.com>
2025-09-10 19:41:19 +0300

    feat(terraform): provision ephemeral EC2 with SG and tags with the default vpc

---
+++ terraform/main.tf
@@ -0,0 +1,63 @@
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get default subnet in AZ a
+data "aws_subnet" "default_a" {
+  availability_zone = "eu-central-1a"
+  default_for_az    = true
+}
+
+# Create security_group
+resource "aws_security_group" "ci_ssh" {
+  name        = "ci-ssh-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+# EC2 Instance
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "ci_ephemeral" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  subnet_id              = data.aws_subnet.default_a.id
+  vpc_security_group_ids = [aws_security_group.ci_ssh.id]
+  key_name               = var.ssh_key_name
+
+  tags = {
+    Name = "ci_ephemeral"
+    lifespan= "ephemeral"
+    owner= var.ci_owner
+  }
+}
